197ffa2c4fe8a2e07fdbb11cfb56099c68f3ff3d
danezis kevin <kdanezis@gmail.com>
2023-04-02 21:38:15 +0200

    export searchEngine

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -2,12 +2,8 @@
     import { browser } from '$app/environment';
 	import { jsonDataStore } from '$lib/store';
 	import FilterBlock from './FilterBlock.svelte'
+    import { SearchEngine } from './searchEngine';
     import Upload from './Upload.svelte';
-
-	const ID_ROYAUMES = 'royaumes'
-	const ID_SUBROYAUMES = 'subRoyaumes'
-	const ID_PROTOCOLES = 'protocoles'
-	const ID_ENVS = 'envs'
 
 	//Filters
 	let fRoyaumes:string[] = []
@@ -15,6 +11,10 @@
 	let fProtocols:string[] = []
 	let fEnvs:string[] = []
 	let hideAll:boolean = false //Set to true if nothing is to be shown
+
+	//Counter of ClientIds
+    const ID_ALL = 'id_all'
+    let mapCounter = new Map<string, number>()
 
 	//Representation of the JSON data structure
 	let royaumes:{ 
@@ -35,16 +35,14 @@
 					}[]; 
 				}[]; 
 			}[] = []
-
-	let start = new Date()
-	let end:Date;
 	
 	if($jsonDataStore.length > 100){
 		royaumes = JSON.parse($jsonDataStore) 
+		updateFilters()
+		updateCounters()
 	} 
 
 	function updateFilters(){
-		start = new Date()
 		fRoyaumes = []
 		royaumes.forEach((royaume) => {
 			fRoyaumes.push(royaume.royaume)
@@ -70,209 +68,61 @@
 		fSubs = fSubs.filter((value, index, array) => array.indexOf(value) === index).sort()
 		fProtocols = fProtocols.filter((value, index, array) => array.indexOf(value) === index).sort()
 		fEnvs = fEnvs.filter((value, index, array) => array.indexOf(value) === index).sort()
-
-		console.info("updating Filter ended in " + ((new Date()).getMilliseconds() - start.getMilliseconds()) + "ms")
 	}
 
-	let previousSearchValue:string
+	let currentSearchValue:string
 	function renderSearch(event:Event){
 
 		let searchValue = (document.getElementById('search') as HTMLInputElement)?.value
 		searchValue = searchValue.trim()
-		if(previousSearchValue == searchValue){
+		if(currentSearchValue == searchValue){
 			return
 		}
-		previousSearchValue = searchValue
-		render(event)
+		currentSearchValue = searchValue
+		render()
 	}
 
-	function render(event:Event){
-		start = new Date()
-		hideAll = false
-		//Raw search inside the json directly
-		if(previousSearchValue !== undefined && !$jsonDataStore.includes(previousSearchValue)){
-			royaumes.forEach((royaume) => {
-				royaume.show = false
-			})
-			hideAll = true
-		} else {
-			//Filtering with side applet
-			let map = new Map<string,Map<string,boolean>>()
-			map.set(ID_ROYAUMES, getStateOfFilters(ID_ROYAUMES))
-			map.set(ID_SUBROYAUMES, getStateOfFilters(ID_SUBROYAUMES))
-			map.set(ID_PROTOCOLES, getStateOfFilters(ID_PROTOCOLES))
-			map.set(ID_ENVS, getStateOfFilters(ID_ENVS))
-	
-			
-			royaumes.forEach((royaume) => {
-				
-				royaume.show = (map.get(ID_ROYAUMES)?.get(royaume.royaume) === true)
+	function render(){
+		//Refresh state of store
+		royaumes = SearchEngine.render(royaumes, currentSearchValue, $jsonDataStore)
+		hideAll = SearchEngine.HIDE_ALL
+		updateCounters()
+	}
+
+
+	function markerHtml(source:string){
+		return source.replace(currentSearchValue, '<span class="found">' + currentSearchValue + '</span>') 
+	}
+
+
+	function updateCounters(){
+
+		let clientIdCounter = 0
+		let allClientIdCounter = 0
+
+		royaumes.forEach((royaume) => {
+			clientIdCounter=0
+			if(royaume.show !== false){
 				royaume.subs.forEach((sub) => {
-					
-					sub.show = (map.get(ID_SUBROYAUMES)?.get(sub.sub) === true)
-					sub.clientIds.forEach((clientId) => {
-						
-						
-						clientId.show = (map.get(ID_PROTOCOLES)?.get(clientId.protocol) === true)
-						clientId.envs.forEach((env) => {
+
+					if(sub.show !== false){
+						sub.clientIds.forEach((clientId) => {
 							
-							env.show = (map.get(ID_ENVS)?.get(env.env) === true)
-							if(env.redirectUris){
-								env.redirectUris.forEach((uri) => {
-								})
+							if(clientId.show !== false){
+								clientIdCounter++
+								allClientIdCounter++
 							}
 						})
-					})
-				});
-			});
-
-			let oneRoyaumeWasFound = false
-			let oneSubWasFound = false
-			let oneClientIdWasFound = false
-			let oneEnvWasFound = false
-			let oneUriWasFound = false
-
-			if(previousSearchValue !== undefined && previousSearchValue !== ""){
-				//Filtering with text content
-				royaumes.forEach((royaume) => {
-					
-					oneSubWasFound = false
-					if(royaume.show !== false){
-						
-						if(royaume.royaume.indexOf(previousSearchValue) !== -1){
-							royaume.show = true
-							oneRoyaumeWasFound = true
-							console.debug(royaume.royaume + ' was found matching (royaume)')
-						} else {
-							royaume.subs.forEach((sub) => {
-								oneClientIdWasFound=false
-								if(sub.show !== false){
-									
-									if(sub.sub.indexOf(previousSearchValue) !== -1){
-										sub.show = true
-										oneSubWasFound = true
-										console.debug(sub.sub + ' was found matching (sub)')
-									} else {
-										sub.clientIds.forEach((clientId) => {
-											oneEnvWasFound = false
-											if(clientId.show !== false){
-												
-												if(clientId.clientId.indexOf(previousSearchValue) !== -1){
-													clientId.show = true
-													oneClientIdWasFound = true
-													console.debug(clientId.clientId + ' was found matching (clientId)')
-												} else {
-													clientId.envs.forEach((env) => {
-														oneUriWasFound = false
-														if(env.show !== false){
-
-															if(env.redirectUris){
-																env.redirectUris.forEach((uri) => {
-																	if(uri.indexOf(previousSearchValue) !== -1){
-																		console.debug(uri + ' was found matching (uri)')
-																		oneUriWasFound=true
-																	}
-																})
-																env.show = oneUriWasFound
-																oneEnvWasFound = oneUriWasFound
-															}
-														}
-													})
-													clientId.show = oneEnvWasFound
-													oneClientIdWasFound = oneClientIdWasFound || oneEnvWasFound
-												}
-											}
-										})
-										sub.show = oneClientIdWasFound
-										oneSubWasFound = oneSubWasFound || oneClientIdWasFound
-									}
-								}
-							})
-							royaume.show = oneSubWasFound
-							oneRoyaumeWasFound = oneRoyaumeWasFound || oneSubWasFound
-						}
 					}
 				})
 			}
+			mapCounter.set(royaume.royaume, clientIdCounter)
+		})
+		mapCounter.set(ID_ALL, allClientIdCounter)	
 
-			//Propagation to Parents if all children's nodes are hidden
-			let OneRoyaumesisShow = false
-			let OneSubRoyaumesisShow = false
-			let OneClientIdisShow = false
-			let OneEnvisShow = false
-			royaumes.forEach((royaume) => {
-				if(royaume.show !== false){
-					royaume.subs.forEach((sub) => {
-
-						if(sub.show !== false){
-							sub.clientIds.forEach((clientId) => {
-								
-								if(clientId.show !== false){
-									clientId.envs.forEach((env) => {
-
-										/*if(env.show !== false){
-											if(env.redirectUris){
-												env.redirectUris.forEach((uri) => {
-												})
-											}
-										}*/
-										OneEnvisShow = OneEnvisShow || env.show
-									})
-									if(clientId.clientId.indexOf(previousSearchValue) !== -1){
-										clientId.show = true
-									} else if(!OneEnvisShow){
-										clientId.show = false
-									}
-								}
-								OneClientIdisShow = OneClientIdisShow || clientId.show
-							})
-							if(sub.sub.indexOf(previousSearchValue) !== -1){
-								sub.show = true
-							} else if(!OneClientIdisShow){
-								sub.show = false
-							}
-						}
-						OneSubRoyaumesisShow = OneSubRoyaumesisShow || sub.show
-					})
-
-					if(royaume.royaume.indexOf(previousSearchValue) !== -1){
-						royaume.show = true
-					} else if(!OneSubRoyaumesisShow){
-						royaume.show = false
-					}
-				}
-				
-				OneRoyaumesisShow = OneRoyaumesisShow || royaume.show
-			})
-
-			hideAll = !OneRoyaumesisShow
-
-		}
-
-		//Refresh state of store
-		royaumes = royaumes
-		console.info("rendering ended in " + ((new Date()).getMilliseconds() - start.getMilliseconds()) + "ms")
-		return
-			
+		console.info(mapCounter)
 	}
 
-
-	function getStateOfFilters(id:string):Map<string,boolean>{
-		let map = new Map<string, boolean>()
-		let inputs = document.getElementById('filterFor'+id)?.getElementsByTagName("input") 
-		if(inputs !== undefined){
-			for(let item of inputs){
-				map.set(item.value, item.checked)
-			}
-		}
-		return map
-	}
-	
-	updateFilters()
-
-	function markerHtml(source:string){
-		return source.replace(previousSearchValue, '<span class="found">' + previousSearchValue + '</span>') 
-	}
 </script>
 
 <svelte:head>
@@ -288,10 +138,10 @@
 	<side>
 		<h2>Filtres</h2>
 
-		<FilterBlock filterCode={ID_ROYAUMES} filterTitre='Royaumes' filterList={fRoyaumes}  render={render} />
-		<FilterBlock filterCode={ID_SUBROYAUMES} filterTitre='Sous-Royaumes' filterList={fSubs}  render={render} />
-		<FilterBlock filterCode={ID_PROTOCOLES} filterTitre='Protocoles' filterList={fProtocols}  render={render} />
-		<FilterBlock filterCode={ID_ENVS} filterTitre='Environnements' filterList={fEnvs}  render={render} />
+		<FilterBlock filterCode={SearchEngine.ID_ROYAUMES} filterTitre='Royaumes' filterList={fRoyaumes}  render={render} />
+		<FilterBlock filterCode={SearchEngine.ID_SUBROYAUMES} filterTitre='Sous-Royaumes' filterList={fSubs}  render={render} />
+		<FilterBlock filterCode={SearchEngine.ID_PROTOCOLES} filterTitre='Protocoles' filterList={fProtocols}  render={render} />
+		<FilterBlock filterCode={SearchEngine.ID_ENVS} filterTitre='Environnements' filterList={fEnvs}  render={render} />
 		<button on:click="{() => $jsonDataStore = ''}">clear localStorage</button>
 	</side>
 
@@ -299,11 +149,11 @@
 		<h2>Data</h2>
 
 		<input type='text' id='search' placeholder='Start typing to filtering...' on:keydown={renderSearch} on:keyup={renderSearch} on:change={renderSearch}/>
-		
 		{#if royaumes.length > 0 }
-		<ul>
+		{#key royaumes}{mapCounter.get(ID_ALL)} IdClients affichés{/key}
+		
 			{#each royaumes as royaume}
-				<li class:hide={royaume.show !== null && royaume.show === false}>{@html markerHtml(royaume.royaume)}<ul>
+				<div class:hide={royaume.show !== null && royaume.show === false}><h3>{@html markerHtml(royaume.royaume)} - {mapCounter.get(royaume.royaume)}</h3><ul>
 						{#each royaume.subs as sub}
 						<li class:hide={sub.show !== null && sub.show === false}>{@html markerHtml(sub.sub)}<ul>
 								{#each sub.clientIds as clientId}
@@ -321,9 +171,9 @@
 								{/each}
 						</ul></li>
 						{/each}
-				</ul></li>
+				</ul></div>
 			{/each}
-		</ul>
+		
 		{/if}
 
 		{#if hideAll }
---
+++ src/routes/searchEngine.ts
@@ -0,0 +1,208 @@
+export module SearchEngine{
+    
+
+	export const ID_ROYAUMES = 'royaumes'
+	export const ID_SUBROYAUMES = 'subRoyaumes'
+	export const ID_PROTOCOLES = 'protocoles'
+	export const ID_ENVS = 'envs'
+
+    export let HIDE_ALL = false
+    
+    export function render(royaumes:{ 
+        royaume: string;
+        show: boolean; 
+        subs: { 
+            sub: string; 
+            show: boolean;
+            clientIds: { 
+                protocol: string; 
+                show: boolean;
+                clientId: string;
+                envs: { 
+                    env: string; 
+                    show: boolean;
+                    redirectUris: string[]; 
+                }[]; 
+            }[]; 
+        }[]; 
+    }[], currentSearchValue:string, rawData:string ){
+		let start = new Date()
+		HIDE_ALL = false
+		//Raw search inside the json directly
+		if(currentSearchValue !== undefined && !rawData.includes(currentSearchValue)){
+			royaumes.forEach((royaume) => {
+				royaume.show = false
+			})
+			HIDE_ALL = true
+		} else {
+			//Filtering with side applet
+			let map = new Map<string,Map<string,boolean>>()
+			map.set(ID_ROYAUMES, getStateOfFilters(ID_ROYAUMES))
+			map.set(ID_SUBROYAUMES, getStateOfFilters(ID_SUBROYAUMES))
+			map.set(ID_PROTOCOLES, getStateOfFilters(ID_PROTOCOLES))
+			map.set(ID_ENVS, getStateOfFilters(ID_ENVS))
+	
+			
+			royaumes.forEach((royaume) => {
+				
+				royaume.show = (map.get(ID_ROYAUMES)?.get(royaume.royaume) === true)
+				royaume.subs.forEach((sub) => {
+					
+					sub.show = (map.get(ID_SUBROYAUMES)?.get(sub.sub) === true)
+					sub.clientIds.forEach((clientId) => {
+						
+						
+						clientId.show = (map.get(ID_PROTOCOLES)?.get(clientId.protocol) === true)
+						clientId.envs.forEach((env) => {
+							
+							env.show = (map.get(ID_ENVS)?.get(env.env) === true)
+							if(env.redirectUris){
+								env.redirectUris.forEach((uri) => {
+								})
+							}
+						})
+					})
+				});
+			});
+
+			let oneRoyaumeWasFound = false
+			let oneSubWasFound = false
+			let oneClientIdWasFound = false
+			let oneEnvWasFound = false
+			let oneUriWasFound = false
+
+			if(currentSearchValue !== undefined && currentSearchValue !== ""){
+				//Filtering with text content
+				royaumes.forEach((royaume) => {
+					
+					oneSubWasFound = false
+					if(royaume.show !== false){
+						
+						if(royaume.royaume.indexOf(currentSearchValue) !== -1){
+							royaume.show = true
+							oneRoyaumeWasFound = true
+							console.debug(royaume.royaume + ' was found matching (royaume)')
+						} else {
+							royaume.subs.forEach((sub) => {
+								oneClientIdWasFound=false
+								if(sub.show !== false){
+									
+									if(sub.sub.indexOf(currentSearchValue) !== -1){
+										sub.show = true
+										oneSubWasFound = true
+										console.debug(sub.sub + ' was found matching (sub)')
+									} else {
+										sub.clientIds.forEach((clientId) => {
+											oneEnvWasFound = false
+											if(clientId.show !== false){
+												
+												if(clientId.clientId.indexOf(currentSearchValue) !== -1){
+													clientId.show = true
+													oneClientIdWasFound = true
+													console.debug(clientId.clientId + ' was found matching (clientId)')
+												} else {
+													clientId.envs.forEach((env) => {
+														oneUriWasFound = false
+														if(env.show !== false){
+
+															if(env.redirectUris){
+																env.redirectUris.forEach((uri) => {
+																	if(uri.indexOf(currentSearchValue) !== -1){
+																		console.debug(uri + ' was found matching (uri)')
+																		oneUriWasFound=true
+																	}
+																})
+																env.show = oneUriWasFound
+																oneEnvWasFound = oneUriWasFound
+															}
+														}
+													})
+													clientId.show = oneEnvWasFound
+													oneClientIdWasFound = oneClientIdWasFound || oneEnvWasFound
+												}
+											}
+										})
+										sub.show = oneClientIdWasFound
+										oneSubWasFound = oneSubWasFound || oneClientIdWasFound
+									}
+								}
+							})
+							royaume.show = oneSubWasFound
+							oneRoyaumeWasFound = oneRoyaumeWasFound || oneSubWasFound
+						}
+					}
+				})
+			}
+
+			//Propagation to Parents if all children's nodes are hidden
+			let OneRoyaumesisShow = false
+			let OneSubRoyaumesisShow = false
+			let OneClientIdisShow = false
+			let OneEnvisShow = false
+			royaumes.forEach((royaume) => {
+				if(royaume.show !== false){
+					royaume.subs.forEach((sub) => {
+
+						if(sub.show !== false){
+							sub.clientIds.forEach((clientId) => {
+								
+								if(clientId.show !== false){
+									clientId.envs.forEach((env) => {
+
+										/*if(env.show !== false){
+											if(env.redirectUris){
+												env.redirectUris.forEach((uri) => {
+												})
+											}
+										}*/
+										OneEnvisShow = OneEnvisShow || env.show
+									})
+									if(clientId.clientId.indexOf(currentSearchValue) !== -1){
+										clientId.show = true
+									} else if(!OneEnvisShow){
+										clientId.show = false
+									}
+								}
+								OneClientIdisShow = OneClientIdisShow || clientId.show
+							})
+							if(sub.sub.indexOf(currentSearchValue) !== -1){
+								sub.show = true
+							} else if(!OneClientIdisShow){
+								sub.show = false
+							}
+						}
+						OneSubRoyaumesisShow = OneSubRoyaumesisShow || sub.show
+					})
+
+					if(royaume.royaume.indexOf(currentSearchValue) !== -1){
+						royaume.show = true
+					} else if(!OneSubRoyaumesisShow){
+						royaume.show = false
+					}
+				}
+				
+				OneRoyaumesisShow = OneRoyaumesisShow || royaume.show
+			})
+
+			HIDE_ALL = !OneRoyaumesisShow
+
+		}
+
+		
+		console.info("rendering ended in " + ((new Date()).getMilliseconds() - start.getMilliseconds()) + "ms")
+		return royaumes
+			
+	}
+
+
+	function getStateOfFilters(id:string):Map<string,boolean>{
+		let map = new Map<string, boolean>()
+		let inputs = document.getElementById('filterFor'+id)?.getElementsByTagName("input") 
+		if(inputs !== undefined){
+			for(let item of inputs){
+				map.set(item.value, item.checked)
+			}
+		}
+		return map
+	}
+}
