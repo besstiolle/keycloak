17247dab10c4c64dd9d377f29dae775a848e0234
Kevin Danezis <contact@kdanezis.fr>
2023-04-12 08:52:45 +0200

    :sparkles: add new dummy page

--- src/routes/Header.svelte
+++ src/routes/Header.svelte
@@ -11,7 +11,6 @@
 		</a>
 	</div>
 
-	<!--
 	<nav>
 		<svg viewBox="0 0 2 3" aria-hidden="true">
 			<path d="M0,0 L1,2 C1.5,3 1.5,3 2,3 L2,0 Z" />
@@ -21,13 +20,18 @@
 				<a href="/">Home</a>
 			</li>
 		</ul>
+		<ul>
+			<li aria-current={$page.url.pathname === '/charts' ? 'page' : undefined}>
+				<a href="/charts">Charts</a>
+			</li>
+		</ul>
 		<svg viewBox="0 0 2 3" aria-hidden="true">
 			<path d="M0,0 L0,3 C0.5,3 0.5,3 1,2 L2,0 Z" />
 		</svg>
-	</nav> -->
+	</nav> 
 
 	<div class="corner">
-		<a href="https://github.com/sveltejs/kit">
+		<a href="https://github.com/besstiolle/keycloak">
 			<img src={github} alt="GitHub" />
 		</a>
 	</div>
---
+++ src/routes/charts/+page.svelte
@@ -0,0 +1,14 @@
+<script lang="ts">
+
+</script>
+
+<svelte:head>
+	<title>Keycloak</title>
+	<meta name="description" content="Keycloak demo app"/>
+</svelte:head>
+
+<section><h1>Charts (todo)</h1></section>
+
+
+<style>
+</style>
