958551a89666dd6d60f9b69bf9e2a304c6a5323b
Kevin Danezis <contact@kdanezis.fr>
2023-04-13 12:27:13 +0200

    :sparkles: add pie charts

--- src/routes/charts/+page.svelte
+++ src/routes/charts/+page.svelte
@@ -1,10 +1,8 @@
 <script lang="ts">
-    import { browser } from '$app/environment';
-	import Chart from 'chart.js/auto';
-	import 'chartjs-adapter-luxon';
 	import { jsonDataStore } from '$lib/store';
     import type { commit } from '$lib/struct';
-	import { onMount } from 'svelte';
+    import LineCountersAll from './LineCountersAll.svelte';
+    import PieCountersByInstance from './PieCountersByInstance.svelte';
 
 	let allCommits:typeof commit[] = []
 
@@ -20,31 +18,20 @@
 		prepareData()
 	}
 	
-
+	let countersByInstance = new Map<string,number>()
+	let countersByInstanceAndRoyaume = new Map<string,Map<string, number>>()
 	let countersByTs = new Map<number,number>()
 	let countersByTsMin = 100000000
 	let countersByTsMax = 0
-	const padL = (nr:number, len = 2, chr = `0`) => `${nr}`.padStart(2, chr);
 
 	initiateJson();
 
-
-	function formatDate(ts:number){
-		const dt = new Date(ts*1000)
-		return `${
-			//dt.getFullYear()}/${
-			padL(dt.getDate())}/${
-			padL(dt.getMonth()+1)}  ${
-			padL(dt.getHours())}:${
-			padL(dt.getMinutes())}:${
-			padL(dt.getSeconds())}`
-	}
-
 	function prepareData(){
-
+		
 		let countersByInstanceByTs = new Map<number,Map<string, number>>()
-		let countersByInstance = new Map<string,number>()
+		let countersByRoyaumeForInstance = new Map<string, number>()
 		let counterByInstance:number
+		let counterByRoyaumeForInstance:number
 
 		countersByTs = new Map<number,number>()
 		let counter:number
@@ -54,13 +41,18 @@
 			countersByInstance = new Map<string,number>()
 			commit.instances.forEach((instance) => {
 				counterByInstance=0
+				countersByRoyaumeForInstance = new Map<string, number>()
 				instance.royaumes.forEach((royaume) => {
+					counterByRoyaumeForInstance = 0
 					royaume.clientIds.forEach((clientId) => {
 						counterByInstance++
+						counterByRoyaumeForInstance++
 						counter++
 					})
+					countersByRoyaumeForInstance.set(royaume.label,counterByRoyaumeForInstance)
 				})
 				countersByInstance.set(instance.label, counterByInstance)
+				countersByInstanceAndRoyaume.set(instance.label, countersByRoyaumeForInstance)
 			})
 			countersByInstanceByTs.set(commit.ts, countersByInstance)
 			countersByTs.set(commit.ts, counter)
@@ -74,61 +66,6 @@
 		
 	}
 	
-	function startChart(st?:string){
-		new Chart(
-			document.getElementById('acquisitions') as HTMLCanvasElement,
-			{
-				type: 'line',
-				
-				options: {
-					animation: {
-						duration: 0
-					},
-					plugins: {
-						title: {
-							display: true,
-							text: 'Evolution des clientsIds'
-						}
-					},
-					scales: {
-						x: {
-							type: 'time',
-							time: {
-								// Luxon format string
-								tooltipFormat: 'DD T'
-							},
-							title: {
-								display: true,
-								text: 'Date'
-							},
-							ticks : {
-								maxTicksLimit:10
-							}
-						},
-						y: {
-							min: Math.round(countersByTsMin / 1.1),
-							max: Math.round(countersByTsMax * 1.1),
-						}
-					},
-					
-				},
-				data: {
-					labels: Array.from(countersByTs.keys()).sort().map(row => row * 1000),
-					datasets: [
-					{
-						label: 'ClientIds',
-						//data: Array.from(countersByTs.keys()).sort().map(row => {x:formatDate(row);y:countersByTs.get(row)} )
-						data: Array.from(countersByTs.keys()).sort().map(row => countersByTs.get(row) )
-					}
-					]
-				}
-			}
-		);
-		
-	}
-
-	onMount(() => startChart())
-	
 </script>
 
 <svelte:head>
@@ -137,16 +74,22 @@
 </svelte:head>
 
 
-<section><h1>Charts (in progress)</h1></section>
+<section><h1>Charts</h1></section>
 
 <div class="chart-container">
-	<canvas id="acquisitions"></canvas>
+	<LineCountersAll countersByTs={countersByTs} countersByTsMin={countersByTsMin} countersByTsMax={countersByTsMax}/>
+</div>
+<div class="chart-container">
+	<PieCountersByInstance countersByInstance={countersByInstance} countersByInstanceAndRoyaume={countersByInstanceAndRoyaume}/>
 </div>
 
 <style>
 .chart-container{
-	height: 60vh;
-	width: 60vw;
-	position: relative
+	height: 80vh;
+	width: 100%;
+	margin-bottom: 20vh;
+}
+:global(.marginAuto){
+	margin:auto;
 }
 </style>
---
+++ src/routes/charts/PieCountersByInstance.svelte
@@ -0,0 +1,103 @@
+
+<script lang="ts">
+	import Chart from 'chart.js/auto';
+	import { onMount } from 'svelte';
+    export let countersByInstance:Map<string,number>
+    export let countersByInstanceAndRoyaume:Map<string,Map<string, number>>
+
+    const HTMLCanvasElementID = 'pieCountersByInstanceCanevas'
+    const COLORS = [
+                'rgba(71, 96, 136, %o)', 
+                'rgba(255, 197, 98, %o)', 
+                'rgba(255, 109, 116, %o)',
+                'rgba(79, 221, 195, %o)',
+                'rgba(97, 168, 232, %o)',
+            ]
+    const DATA = {
+        labels: toFlatArray().labels,
+        datasets: [{
+                label: "ClientId de l'instance",
+                data: Array.from(countersByInstance.keys()).map(row => countersByInstance.get(row)),
+                backgroundColor: COLORS.map(color => color.replace('%o', '1')),
+            },{
+                label: "ClientId du royaume",
+                data: toFlatArray().values,
+                backgroundColor: toFlatArray().colors,
+            }
+        ]};
+
+    function toFlatArray(){
+        let values:number[] = []
+        let labels:string[] = []
+        let colors:string[] = []
+        let currentColor:string
+        let sizeOfRoyaume:number
+        let opacity:number
+        
+
+        let j=0
+        let k=1
+
+        //Padding to fix multi series of pie Charts
+        Array.from(countersByInstance.keys()).map(row => {
+            
+            labels.push(row + ' (' + (countersByInstance.get(row) as number) + ')')
+        })
+        Array.from(countersByInstance.keys()).map(() => values.push(0))
+        Array.from(countersByInstance.keys()).map(() => colors.push("#000"))
+
+
+        Array.from(countersByInstanceAndRoyaume.keys()).map(labelInstance => {
+            
+            currentColor = COLORS[j]
+            let counterByRoyaumeForInstance = countersByInstanceAndRoyaume.get(labelInstance)
+            if(counterByRoyaumeForInstance){
+                sizeOfRoyaume = counterByRoyaumeForInstance.size+1
+                k=1
+                Array.from(counterByRoyaumeForInstance.keys()).map(labelRoyaume => {
+                    let value = counterByRoyaumeForInstance?.get(labelRoyaume)
+                    if(value){
+                        values.push(value)
+                        labels.push(labelRoyaume + ' (' + value + ')')
+                        opacity = Math.round(100/sizeOfRoyaume * k)/100
+                        colors.push(currentColor.replace('%o', opacity+''))
+                    }
+                    k++
+                })
+            }
+            j++
+        })
+        return {values:values, labels:labels, colors:colors}
+    }
+
+	function startChart(){
+		new Chart(
+			document.getElementById(HTMLCanvasElementID) as HTMLCanvasElement,
+			{
+				type: 'doughnut',
+				options: {
+					animation: {
+						duration: 1000
+					},
+					plugins: {
+						legend: {
+                            position: 'right',
+                        }
+					},					
+				},
+				data:DATA
+			}
+		);
+        (document.getElementById(HTMLCanvasElementID) as HTMLCanvasElement).classList.add('marginAuto')
+	}
+
+    //Starting graph as soon as the dom is rendered
+	onMount(() => startChart())
+	
+</script>
+
+<h2>Ratio des ClientsIds par instance et par royaume</h2>
+<canvas id={HTMLCanvasElementID}></canvas>
+
+<style>
+</style>
